af1d6cb1753846e269fe5c5a2bd2fab1e64dfc6c
uni <unibtc@gmail.com>
2018-02-27 17:47:22 +0800

    store on config file instead of process.env

--- build/checkRpcSecure.js
+++ build/checkRpcSecure.js
@@ -1,9 +1,19 @@
 const readlineSync = require('readline-sync');
+const fs = require('fs-extra');
+const rpcpath = `${process.env.CGDIR}/src/config/general/rpc.js`;
 
-module.exports = () => {
-  if (readlineSync.keyInYN('Would you like to secure the rpc connection?')) {
-    process.env.RPCUSER = readlineSync.question('Enter your desired rpc username: ');
-    process.env.RPCPW = readlineSync.question('Enter your desired rpc password: ');
-  }
+const _setRpc = exports.setRpc = async () => {
+  const rpcuser = readlineSync.question('Enter your desired rpc username: ');
+  const rpcpass = readlineSync.question('Enter your desired rpc password: ');
+  await fs.outputFile(rpcpath, `module.exports = {rpcuser:'${rpcuser}',rpcpass:'${rpcpass}'}`);
   process.stdout.write('\x1B[2J\x1B[0f');
+  return true;
+};
+exports.check = async () => {
+  if (!await fs.pathExists(rpcpath)) {
+    if (readlineSync.keyInYN('Would you like to secure the rpc connection?')) {
+      return await _setRpc();
+    }
+  }
+  return true;
 };
