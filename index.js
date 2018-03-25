16fc3f4fae012cf70e1c9fbc38763d89d0d71a6e
uni <unibtc@gmail.com>
2018-03-25 20:12:33 +0800

    run compress logs every 12hrs

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const colors = {
 };
 require('console-stamp')(console, { pattern: 'mm-dd-yyyy HH:MM:ss', colors });
 const schedule = require('node-schedule');
+const logtar = require('src/lib/logtar');
 if (!process.env.CGDIR) {
   console.error('Please start the wallet service via "coinglue start" command.');
   process.exit();
@@ -23,6 +24,14 @@ const j = schedule.scheduleJob('* * * * *', () => {
     global.gc();
   } else {
     j.cancel();
+  }
+});
+const logrotate = schedule.scheduleJob('0 */12 * * *', async () => {
+  try {
+    await logtar();
+  } catch (err) {
+    console.error(err.stack || err.message);
+    logrotate.cancel();
   }
 });
 require('src/app').start().then(() => {}).catch((err) => {
