27f88f1410186a59c09c9fbcc34daf91b959bb26
uni <unibtc@gmail.com>
2018-04-11 00:35:08 +0800

    use postgresql on addressmanager, and drop lmdb

--- src/services/addressManager.js
+++ src/services/addressManager.js
@@ -1,4 +1,4 @@
-const DB = require('./db');
+const db = require('src/db');
 const coinjs = require('coinjs-lib');
 const config = require('src/config');
 const addrUtils = require('./utils');
@@ -16,12 +16,9 @@ class AddrManager {
     if (this.isopen === false) {
       this.netw = addrUtils.getNetwork(this.coin);
       this.rootNode = coinjs.HDNode.fromSeedBuffer(seed, this.netw);
-
       this.bip44 = addrUtils.getConstant(this.coin);
       this.coinname = this.coin == 'tbtc' ? 'Testnet' : getName(this.coin);
-      this.dbdir = config.get(`coins.${this.coin}.addressDb`);
       this.addrType = config.get(`coins.${this.coin}.type`);
-      this.db = DB(this.dbdir, this.coin);
       this.isopen = true;
     }
     return this;
@@ -51,50 +48,36 @@ class AddrManager {
   getAddressPair (accid, index) {
     return this._deriveAddress(accid, index);
   }
-  getNewAddress (accid) {
+  async getNewAddress (accid) {
     let index;
-    let lastIndex = this.db.get('addressIndex', accid, 'number');
+    let { lastIndex } = await db.getAccAddrIndex(accid, this.coin);
     if (lastIndex == null) {
       index = 0;
     } else {
       index = lastIndex + 1;
     }
     const { address, privkey } = this.getAddressPair(accid, index);
-    this.db.set('address', address, { accountId: accid, index });
-    this.db.set('addressIndex', accid, index);
-    this.db.set('addressMap', `acc${accid}_i${index}`, address);
-
     return { address, privkey, index };
   }
-  getAddressInfo (address) {
-    return this.db.get('address', address);
+  async getAddressInfo (address) {
+    return await db.getAddrInfoByCoin(address, this.coin);
   }
-  getAddress (accid) {
-    let lastIndex = this.db.get('addressIndex', accid, 'number');
+  async getAddress (accid) {
+    let { lastIndex } = await db.getAccAddrIndex(accid, this.coin);
     if (lastIndex == null) {
       return false;
     }
-    const addr = this.db.get('addressMap', `acc${accid}_i${lastIndex}`);
     const addrPair = this.getAddressPair(accid, lastIndex);
-
-    return { address: addr, privkey: addrPair.privkey, index: lastIndex };
+    return { address: addrPair.address, privkey: addrPair.privkey, index: lastIndex };
   }
-  getAllAddress () {
-    return this.db.getAll('addressMap');
+  async getAllAddress () {
+    return await db.getAllAddressByCoin(this.coin);
   }
-  validateAddress (address) {
-    return this.db.has('addressMap', address);
+  async validateAddress (address) {
+    return await db.addressesExists(address, this.coin);
   }
-  getAccount (accid, num = 10) {
-    let payload = [];
-    const lastIndex = this.db.get('addressIndex', accid, 'number');
-    if (lastIndex === null) {
-      return payload;
-    }
-    for (let i = lastIndex; i > lastIndex - num && i >= 0; i--) {
-      const address = this.db.get('addressMap', `acc${accid}_i${i}`);
-      payload.push({ address, index: i });
-    }
+  async getAccount (accid, num = 10) {
+    const payload = await db.getAccountAddresses(accid, this.coin, num);
     return {
       payload
     };
