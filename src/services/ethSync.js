a3d02a1af2e9bc59b4a1ec8315620c0881ae816e
uni <unibtc@gmail.com>
2018-04-13 21:01:48 +0800

    send hook if tx is found, store hook args incase hook failed, and retry.

--- src/services/ethSync.js
+++ src/services/ethSync.js
@@ -1,12 +1,16 @@
 const config = require('src/config');
+const needle = require('needle');
+const crypto = require('crypto');
 const ethRpc = config.get('coins.eth.rpc');
 const Api = require('@parity/api');
 const provider = new Api.Provider.Ws(`ws://${ethRpc.host}:${ethRpc.port}`);
 const api = new Api(provider);
 
+const hook = config.get('hook');
 const fs = require('fs-extra');
 const { resolve } = require('path');
 const db = require('src/db');
+let txQue = [];
 
 const Promise = require('bluebird');
 const readFile = Promise.promisify(fs.readFile);
@@ -35,13 +39,58 @@ const getLatestBlock = async () => {
 };
 const saveBlock = async blknum => await fs.outputFile(blockPath, parseInt(blknum));
 
+const sendHook = (tx, address, amount) => {
+  const postData = JSON.stringify({ tx, address, amount });
+  const hmacSig = crypto.createHmac('sha256', hook.pass).update(postData).digest('hex');
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(postData),
+      'X-Coinworks-Sig': hmacSig,
+      'User-Agent': 'Coinworks-WebHook'
+    }
+  };
+
+  needle.post(`${hook.host}/eth`, postData, options, (err, resp) => {
+    let isFail = false;
+    if (err || resp.statusCode !== 200) {
+      console.error(`Problem with request: ${err ? err.message : ''} Status: ${resp.statusCode}`);
+      isFail = true;
+    } else {
+      if (typeof resp.body === 'object' && !Array.isArray(resp.body)) {
+        if (!resp.body.success) {
+          console.error(`Failed request: ${JSON.stringify(resp.body)}`);
+          isFail = true;
+        }
+      } else {
+        console.error(`Invalid Response: ${JSON.stringify(resp.body)}`);
+        isFail = true;
+      }
+    }
+    if (isFail == true) {
+      txQue.push({ tx, address, amount });
+    }
+    processQue();
+  });
+};
+
+const processQue = () => {
+  const q = txQue;
+  txQue = [];
+  for (const txs of q) {
+    sendHook(txs.tx, txs.address, txs.amount);
+  }
+};
+
 const syncer = async (blknum) => {
   const islistenting = await api.net.listening();
   if (islistenting) {
     blknum = toHex(blknum);
     let lastBlk = await getLastBlockProcessed();
     lastBlk = !lastBlk ? toHex(blknum) : lastBlk;
+
     let addresses = await db.getAllAddressByCoin('eth');
+
     console.log('Syncing', parseInt(lastBlk), '=>', parseInt(blknum));
     let traceRes = await api.trace.filter({
       'fromBlock': lastBlk,
@@ -54,6 +103,9 @@ const syncer = async (blknum) => {
       txid: tx.transactionHash
     }));
     await saveBlock(blknum);
+    traceRes.forEach(tx => {
+      sendHook(tx.txid, tx.to, tx.amount);
+    });
   } else {
     console.log('Failed to connect to Parity Ws, retrying in 5secs');
     await syncer(blknum);
