684798c37729acd4ed840a814d124bf37d25ffa8
uni <unibtc@gmail.com>
2018-04-13 20:11:05 +0800

    fix ipaddress being mapped as ipv6

--- src/lib/mw.js
+++ src/lib/mw.js
@@ -1,10 +1,11 @@
 const util = require('util');
+const ipaddr = require('ipaddr.js');
 const ipbans = new Map();
 let requestCounter = 0;
 exports.logger = () => async (ctx, next) => {
   const requestId = requestCounter++;
   const remoteAddress = ctx.request.socket.remoteAddress;
-  const ip = ctx.request.ip;
+  const ip = ctx.ip;
   const method = ctx.method;
   const start = new Date();
   console.log(util.format('-> RPC request id=%s remoteip=%s ip=%s method=%s ', requestId, remoteAddress, ip, method));
@@ -17,4 +18,22 @@ exports.logger = () => async (ctx, next) => {
     ipbans.set(ip, ipb === undefined ? 1 : ipb + 1);
   }
   console.log(util.format('<- RPC response id=%s status=%s %s %sms', requestId, ctx.status, ctx.body.error ? ctx.body.error.message : '', start - new Date()));
+};
+const parseIp = ip => {
+  if (!ipaddr.isValid(ip)) {
+    return false;
+  }
+  const addr = ipaddr.parse(ip);
+  if (addr.kind() == 'ipv6') {
+    if (addr.isIPv4MappedAddress()) {
+      return addr.toIPv4Address().toString();
+    }
+  }
+  return addr.toString();
+};
+
+exports.fixIp = () => async (ctx, next) => {
+  ctx.request.socket.remoteAddress = parseIp(ctx.request.socket.remoteAddress);
+  ctx.ip = parseIp(ctx.ip);
+  await next();
 };
